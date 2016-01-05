30f7cfcdc2fbb95ee4468eee83119f7bd80548d4
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-01-05 15:57:55 -0500

    Mobile Fringe 1.0  /application.js

--- application.js
+++ application.js
@@ -3,6 +3,7 @@
 function init(e){
     $('<div class="modal-backdrop custom_backdrop"><img src="http://kodekloud.s3.amazonaws.com/sites/554a79236e6f64713f000000/69e8cd982124dc73de1f5a67a627ee75/loading.gif" class="" alt=""></div>').appendTo(document.body);
     $('#email_form').submit(function(e){
+        $('#sub_btn').attr('disabled', true)
         e.preventDefault();
         data = {};
         data.send_to = "support@mobilefringe.com";
