0cda166a4dce1f2be08afdc260dbf5c630257af2
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-19 12:11:53 -0400

    Mobile Fringe 2.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
-    
+    $('#email_form').submit(function(e){
+        
+        var pathArray = window.location.pathname.split( '/' );
+        var slug = pathArray[pathArray.length-1];
+        prefix = get_prefix();
+        console.log("hello",pathArray,slug,prefix);
+        $('#sub_btn').attr('disabled', true)
+        e.preventDefault();
+        data = {};
+        data.send_to = "contact@mobilefringe.com";
+        data.subject = "Get in touch - Mobile Fringe form";
+        data.body = {"email" : $('#email').val(), "name" : $('#name').val(), "message" : $('#message').val()};
+        $.post('//mobilefringe.mallmaverick.com/send_contact_email', data, function(data, textStatus, jqXHR){
+            if(textStatus == "success"){
+                $('#email_sent').fadeIn();
+                $('#email_form').trigger('reset');
+                $('#sub_btn').removeAttr("disabled");
+            }
+            else{
+                alert("Error sending email. Please try again later.");
+            }
+        });
+    });
 });
