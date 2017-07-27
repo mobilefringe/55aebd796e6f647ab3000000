0674cf7c751908c885326bbfe51a0d0ad721ee6b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-27 10:46:34 -0400

    Mobile Fringe 2.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -39,25 +39,25 @@ $(window).load(function(){
         centerMode: true,
         centerPadding: '60px',
         slidesToShow: 3,
-        responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            arrows: true,
-            centerMode: true,
-            centerPadding: '40px',
-            slidesToShow: 3
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            arrows: true,
-            centerMode: true,
-            centerPadding: '40px',
-            slidesToShow: 1
-          }
-        }
+        // responsive: [
+        // {
+        //   breakpoint: 768,
+        //   settings: {
+        //     arrows: true,
+        //     centerMode: true,
+        //     centerPadding: '40px',
+        //     slidesToShow: 3
+        //   }
+        // },
+        // {
+        //   breakpoint: 480,
+        //   settings: {
+        //     arrows: true,
+        //     centerMode: true,
+        //     centerPadding: '40px',
+        //     slidesToShow: 1
+        //   }
+        // }
         ]
     });
 });
