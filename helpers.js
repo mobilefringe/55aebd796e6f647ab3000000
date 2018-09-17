24550776ed4961664287a8abcbd3ee4af427a393
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-17 12:10:12 -0400

    Mobile Fringe 2.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -5,32 +5,7 @@ $(document).ready(function(){
         slug = "home"
     }
     $("#subject").val("Get in touch - Mobile Fringe form (sent from " +slug + " page )")
-    // $('#email_form').submit(function(e){
-    //     var pathArray = window.location.pathname.split( '/' );
-    //     var slug = pathArray[pathArray.length-1];
-    //     if(slug === "") {
-    //         slug = "home"
-    //     }
-    //     console.log(slug);
-    //     $('#sub_btn').attr('disabled', true)
-    //     e.preventDefault();
-    //     data = {};
-    //     data.send_to = "contact@mobilefringe.com";
-    //     data.subject = "Get in touch - Mobile Fringe form (sent from " +slug + " page )";
-    //     data.body = {"email" : $('#email').val(), "name" : $('#name').val(), "message" : $('#message').val()};
-    //     $.post('//mobilefringe.mallmaverick.com/send_contact_email', data, function(data, textStatus, jqXHR){
-    //         if(textStatus == "success"){
-    //             $('#email_sent').fadeIn();
-    //             $('#email_form').trigger('reset');
-    //             $('#sub_btn').removeAttr("disabled");
-    //         }
-    //         else{
-    //             alert("Error sending email. Please try again later.");
-    //         }
-    //     });
-        
-        
-    // });
+    
     $('#email_form').submit(function(e) {
         values = [];
         values = JSON.stringify($('#email_form').serializeArray());
